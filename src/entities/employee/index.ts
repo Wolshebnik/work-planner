95e5daf5ceb85d54daa615e13fc07c082e919476
Andrii Teslenko <andteslenkocode@gmail.com>
2026-08-18 21:07:02 +0300

    feat(api): add Zod validation for employees and schedule statuses

--- src/entities/employee/index.ts
+++ src/entities/employee/index.ts
@@ -1,3 +1,7 @@
 export { EmployeeCard } from './ui/employee-card';
-
 export { ArchivedEmployeesCard } from './ui/archived-employees-card';
+export {
+  employeeSchema,
+  employeesSchema,
+  type Employee,
+} from './model/schema';
